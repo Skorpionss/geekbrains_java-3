36e581706bf85e9c0926f29f44eb8cb79be5e17f
Maxim <the_prince@sibmail.com>
2021-04-23 15:29:05 +0700

    1. Написать метод, который меняет два элемента массива местами. (массив может быть любого ссылочного типа);

--- src/lesson_1/Main.java
+++ src/lesson_1/Main.java
@@ -2,6 +2,18 @@ package lesson_1;
 
 public class Main {
     public static void main(String[] args) {
+        String[] mass = {"Я первый"," Я второй"};
+        Integer[] intmass = {1,2};
+        for (int i = 0; i< mass.length; i++ ) System.out.println(mass[i]);
+        for (int i = 0; i< intmass.length; i++ ) System.out.println(intmass[i]);
 
+        Array2Places<String> s1 = new Array2Places<>(mass);
+        Array2Places<Integer> i1 = new Array2Places<>(intmass);
+
+        s1.massMestami();
+        i1.massMestami();
+
+        for (int i = 0; i< mass.length; i++ ) System.out.println(mass[i]);
+        for (int i = 0; i< intmass.length; i++ ) System.out.println(intmass[i]);
     }
 }
