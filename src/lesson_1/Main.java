850292abcd6f2bdd2d7ea246846f2c3246a35caa
Maxim <the_prince@sibmail.com>
2021-04-23 15:35:42 +0700

    Улучшения кода в Main для 1 задания;

--- src/lesson_1/Main.java
+++ src/lesson_1/Main.java
@@ -2,10 +2,15 @@ package lesson_1;
 
 public class Main {
     public static void main(String[] args) {
-        String[] mass = {"Я первый"," Я второй"};
-        Integer[] intmass = {1,2};
-        for (int i = 0; i< mass.length; i++ ) System.out.println(mass[i]);
-        for (int i = 0; i< intmass.length; i++ ) System.out.println(intmass[i]);
+        doTask1();
+    }
+
+
+    static void doTask1() {
+        String[] mass = {"Я первый", " Я второй"};
+        Integer[] intmass = {1, 2};
+        for (int i = 0; i < mass.length; i++) System.out.println(mass[i]);
+        for (int i = 0; i < intmass.length; i++) System.out.println(intmass[i]);
 
         Array2Places<String> s1 = new Array2Places<>(mass);
         Array2Places<Integer> i1 = new Array2Places<>(intmass);
@@ -13,7 +18,9 @@ public class Main {
         s1.massMestami();
         i1.massMestami();
 
-        for (int i = 0; i< mass.length; i++ ) System.out.println(mass[i]);
-        for (int i = 0; i< intmass.length; i++ ) System.out.println(intmass[i]);
+        for (int i = 0; i < mass.length; i++) System.out.println(mass[i]);
+        for (int i = 0; i < intmass.length; i++) System.out.println(intmass[i]);
+
     }
+
 }
